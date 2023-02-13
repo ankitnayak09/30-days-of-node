407cf17edd3c01166b64b7ab5337150dbe685526
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-02-13 22:29:06 +0530

    Create serve-mp3.js

---
+++ Day 1/serve-mp3.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var fs = require("fs");
+http.createServer(function (req, res) {
+	console.log("Port Number: 3000");
+	// Change MIME type to 'audio/mp3'
+	res.writeHead(200, { "Content-Type": "audio/mp3" });
+	fs.exists("audio.mp3", function (exists) {
+		if (exists) {
+			var rstream = fs.createReadStream("audio.mp3");
+			rstream.pipe(res);
+		} else {
+			res.end("Its a 404");
+		}
+	});
+}).listen(3000);
